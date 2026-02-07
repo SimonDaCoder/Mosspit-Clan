653746a937db72acfb00b287a1ab4f7cb0d99e88
SimonDaCoder <sisiritter08@gmail.com>
2026-02-07 16:42:32 +0100

    Update main-section.tsx

--- components/main/main-section.tsx
+++ components/main/main-section.tsx
@@ -3,7 +3,7 @@ import LinkList from "./linklist";
 
 const MainSection = () => {
   return (
-    <div className="main-section flex items-center justify-center flex-col gap-8">
+    <div className="main-section flex items-center justify-center flex-col gap-24">
       <img src="/images/mosspit-schriftzug.png" alt="Main Image" className="w-[80vw] object-fit drop-shadow-[0_4px_6px_rgba(0,0,0,0.5)]"/>
       < LinkList />
     </div>
