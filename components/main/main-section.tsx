551528f8160e6745340bde5be48c8a78c2483c04
SimonDaCoder <sisiritter08@gmail.com>
2026-02-07 14:12:31 +0100

    Add mosspit-schriftzug.png
    mosspit-schriftzug.png is not the final img

--- components/main/main-section.tsx
+++ components/main/main-section.tsx
@@ -3,7 +3,7 @@
 const MainSection = () => {
   return (
     <div className="main-section h-[70vh] flex items-center justify-center">
-
+      <img src="/images/main-image.jpg" alt="Main Image" className="w-full h-full object-cover" />
     </div>
   );
 };
